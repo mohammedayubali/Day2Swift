c392c5d56a22fc35833873d32b98708e3e4725d1
MacStudent <macstudent@126-iMac31.local>
2019-02-08 18:50:09 -0500

    String Handling Fuvtion

--- Day2Swift/main.swift
+++ Day2Swift/main.swift
@@ -10,8 +10,22 @@ import Foundation
 
 print("Hello, World!")
 
-var a:Bool
-if(true < false)
-{
-    print("Thanks")
-}
+//var a:Bool
+//if(true < false)
+//{
+    //print("Thanks")
+//}
+var s: String = "Hello"
+var str = String()
+str = "Hello World"
+var p : String
+p = "Call me"
+print(s,str,p)
+print(s.count)
+print(str.description)
+print(s.hasPrefix("Hello"))
+print(str.hasPrefix("Hello"))
+print(p.hasPrefix("Hello"))
+print(str.lowercased())
+print(str.uppercased())
+
