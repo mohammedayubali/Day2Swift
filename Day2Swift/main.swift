2037c04e589dc42acb02c54a9152afe9a514f5ad
MacStudent <macstudent@126-iMac31.local>
2019-02-08 19:36:18 -0500

    Array handling

--- Day2Swift/main.swift
+++ Day2Swift/main.swift
@@ -48,3 +48,40 @@ if (y.contains(-100))
 {
    print("True")
 }
+
+var a = [10,20,30,40,50]
+for x in a
+{
+    print(x)
+    
+}
+print(a.count)
+print(a[0],a[1],a[2],a[3])
+//a[4]=100
+//print(a[4])
+var b = [Int]()
+b.append(100)
+b.append(200)
+b.append(300)
+b += [111,222,333]
+print(b)
+print(b[0],b[1],b[2])
+print(" values of  c array")
+let c = a + b;
+for i in c
+{
+    print(i)
+
+}
+print ("values of c[1..3] array")
+var k = c[1...3]
+print(k)
+k[1] = 2020
+for i in k{
+print(i)
+}
+print(c[1])
+var r = c[2...4]
+print(c)
+print(r)
+
